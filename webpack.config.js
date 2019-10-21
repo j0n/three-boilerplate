f50b93a3b116cb9ce26e4fb3bbef95a334a928c3
Jon Andersson <andersson.jon@gmail.com>
2019-10-21 10:19:27 +0200

    Add support for webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require("@babel/register");
+
+const config = {
+  entry: ['@babel/polyfill','./app/scripts/main.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules : [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        hash: true
+    })
+  ],
+  resolve: {
+    modules: [
+      path.resolve('./app/scripts'),
+      path.resolve('./node_modules')
+    ]
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+    compress: true,
+    port: 9000,
+    open: true,
+    stats: {
+        assets: false,
+        children: false,
+        chunks: false,
+        chunkModules: false,
+        colors: true,
+        entrypoints: false,
+        hash: false,
+        modules: false,
+        timings: false,
+        version: false,
+    }
+  },
+  watch: false,
+  devtool: 'source-map',
+};
+
+module.exports = config;
